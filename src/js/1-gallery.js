81db1ccdb9f18548b47b0fe8dc4f6c7045e33beb
Uzurch1k <galcenkovlad7@gmail.com>
2024-01-24 20:20:50 +0200

    fix

---
+++ src/js/1-gallery.js
@@ -0,0 +1,28 @@
+// ================================================================
+
+const gallery = document.querySelector('.gallery');
+
+function createGalleryItems() {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+          <img class="gallery-image" src="${preview}" alt="${description}" />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
+gallery.innerHTML = createGalleryItems();
+
+// ================================================================
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// ================================================================
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
